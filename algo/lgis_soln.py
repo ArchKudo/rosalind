871e5b88305ffdfc7c109c66f87c864890c61125
Girish Vλas <vgd.vyas17@gmail.com>
2026-04-07 21:43:36 +0530

    Solution by myself including why both conditionals are required

--- algo/lgis_soln.py
+++ algo/lgis_soln.py
@@ -1,46 +1,69 @@
 def lis(sz: int, arr: list[int]) -> list[int]:
+    """
+    The best array holds the longest sequence at that position
+    Initially the maximum length at each position is one i.e itself
+    """
+    best: list[int] = [1] * sz
 
-    if sz == 0:
-        return arr
-
-    dp = [1] * sz
-    prev = [-1] * sz
-
-    for i in range(sz):
-        print(f"i={i}")
-        for j in range(i):
-            print(f"\tj={j}")
-            print(f"\t arr[{j}]={arr[j]} arr[{i}]={arr[i]}")
-            print(f"\t dp[j]={dp[j]} dp[j]+1={dp[j] + 1} dp[i]={dp[i]}")
-            if arr[j] < arr[i] and dp[j] + 1 > dp[i]:
-                dp[i] = dp[j] + 1
-
-                print(f"\t\tdp[{i}]={dp[i]}")
-                print(f"\t\tdp[{j}]={dp[j]}")
-                print(f"\t\tdp[{j}]+1={dp[j] + 1}")
-
-                prev[i] = j
-                print(f"\t\tprev[{i}]={prev[i]}")
-
-    bestend = 0
+    """
+    Previous holds the index like a linked list
+    to the last best sequence
+    so we can stitch all to get the longest increasing sequence
+    """
+    prev: list[int] = [-1] * sz
 
     for i in range(1, sz):
-        if dp[i] > dp[bestend]:
-            bestend = i
+        for j in range(i):
+            bigger = arr[j] < arr[i]
 
-    result = []
-    k = bestend
+            """
+            Assume arr = [1,2,0,3]
+            i = 1
+            best[1] = best[0] + 1 = 2 _ (I)
 
-    print(prev)
-    while k != -1:
-        print(k)
-        result.append(arr[k])
-        k = prev[k]
-        print(f"\t{k}")
+            i = 2
+            arr[i] < arr[j]
+            best[2] = 1 _ (II)
+            for both j = 0,1
 
-    return result[::-1]
+            i = 3
+                j=0 best[3] = best[0] + 1 = 2
+                j=1 best[3] = best[1] + 1 = 3 (from I)
+                j=2 best[3] = best[2] + 1 = 2 (from II)
+
+            We need to exclude cases where the longest sequence gets shorter
+
+            """
+            longer = best[j] + 1 > best[i]
+
+            if bigger and longer:
+                best[i] = best[j] + 1
+                """
+                arr = [5,1,4,2,3]
+                prev[best=4] = j = 3
+                prev[best=3] = j = 1
+                prev[best=1] = j = -1
+
+                stopping after -1 is encountered
+                i = 4 -> 3 -> 1 -> stop
+                arr[i] = 3 -> 2 -> 1 -> stop
+                arr[::-1] = 1,2,3
+                """
+                prev[i] = j
+
+    farthest = max(range(sz), key=lambda i: best[i])
+
+    seq: list[int] = []
+
+    while farthest != -1:
+        seq.append(arr[farthest])
+        farthest = prev[farthest]
+
+    return seq[::-1]
 
 
 if __name__ == "__main__":
     print(lis(5, [5, 1, 4, 2, 3]))
     print(lis(5, [1, 2, 3, 4, 5]))
+    print(lis(5, [1, 2, 8, 4, 5]))
+    print(lis(4, [1, 2, 0, 3]))
