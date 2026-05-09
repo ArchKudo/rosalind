7c2a811e84225254b5cc958b8fc339d46c3aa608
Girish Vλas <vgd.vyas17@gmail.com>
2026-05-10 00:17:44 +0530

    Add frequent words

---
+++ sandiego/frequent_words.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	content, _ := os.ReadFile("frequent_words.txt")
+
+	lst := strings.Split(string(content), "\n")
+
+	hay := lst[0]
+	k, _ := strconv.Atoi(lst[1])
+
+	// var freq map[string]int = {}
+	freq := make(map[string]int, len(hay))
+
+	for i := 0; i < len(hay)-k+1; i++ {
+		kmer := hay[i : i+k]
+
+		if _, ok := freq[kmer]; ok {
+			freq[kmer] += 1
+		} else {
+			freq[kmer] = 1
+		}
+	}
+
+	max := 0
+
+	for _, val := range freq {
+		if val > max {
+			max = val
+		}
+	}
+
+	most := make([]string, 0, len(freq))
+
+	for kmer, val := range freq {
+		if val == max {
+			most = append(most, kmer)
+		}
+	}
+
+	for _, val := range most {
+		fmt.Printf("%s ", val)
+	}
+	fmt.Println()
+
+}
