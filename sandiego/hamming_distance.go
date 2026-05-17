47d8518698779c79ecc0b389730af11ee57fcb91
Girish Vλas <vgd.vyas17@gmail.com>
2026-05-17 17:21:41 +0530

    Approximate match with hamming distance

---
+++ sandiego/hamming_distance.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func Hamming(a string, b string) (int, error) {
+
+	arra := strings.Split(a, "")
+	brra := strings.Split(b, "")
+
+	if len(arra) != len(brra) {
+		return -1, errors.New("Read lengths different!")
+	}
+
+	mismatches := 0
+
+	for i, vala := range arra {
+		bala := brra[i]
+
+		if vala != bala {
+			mismatches += 1
+		}
+	}
+
+	return mismatches, nil
+}
+
+func main() {
+
+	content, err := os.ReadFile("hamming_distance.txt")
+
+	var read, bead string
+	if err == nil {
+		arr := strings.Split(string(content), "\n")
+		read = arr[0]
+		bead = arr[1]
+	} else {
+		read = "GGGCCGTTGGT"
+		bead = "GGACCGTTGAC"
+	}
+
+	dist, _ := Hamming(read, bead)
+	fmt.Println(dist)
+}
